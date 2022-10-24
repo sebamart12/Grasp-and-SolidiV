64da40d4567709e930b86e11c0b6c6e7181d7b90
sebamart12 <93790958+sebamart12@users.noreply.github.com>
2022-10-24 09:00:08 -0300

    Add files via upload

---
+++ PII_Full_GRASP_and_SOLID_4_Start/src/Library/Recipe.cs
@@ -0,0 +1,60 @@
+//-------------------------------------------------------------------------
+// <copyright file="Recipe.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace Full_GRASP_And_SOLID
+{
+    public class Recipe
+    {
+        private IList<Step> steps = new List<Step>();
+
+        public Product FinalProduct { get; set; }
+        public double quantity{get;set;}
+        public Equipment equipment{get;set;}
+        public int time{get;set;}
+        //Agregado por Creator
+        public void AddStep(Product input, double quantity, Equipment equipment, int time)
+        {
+            Step step1 = new Step(FinalProduct,quantity,equipment,time);
+            this.steps.Add(step1);
+        }
+
+        public void RemoveStep(Step step)
+        {
+            this.steps.Remove(step);
+        }
+
+        // Agregado por SRP
+        public string GetTextToPrint()
+        {
+            string result = $"Receta de {this.FinalProduct.Description}:\n";
+            foreach (Step step in this.steps)
+            {
+                result = result + step.GetTextToPrint() + "\n";
+            }
+
+            // Agregado por Expert
+            result = result + $"Costo de producción: {this.GetProductionCost()}";
+
+            return result;
+        }
+
+        // Agregado por Expert
+        public double GetProductionCost()
+        {
+            double result = 0;
+
+            foreach (Step step in this.steps)
+            {
+                result = result + step.GetStepCost();
+            }
+
+            return result;
+        }
+    }
+}
